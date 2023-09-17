07b8d18b5c49037a5adf087bbe4b1a0095ce62ae
Julien Atanassian <julien.atanassian@gmail.com>
2023-09-17 10:44:27 -0400

    add server route

--- weather-ui/src/backend/server.ts
+++ weather-ui/src/backend/server.ts
@@ -5,6 +5,9 @@ import dotenv from 'dotenv';
 import fastify from 'fastify';
 import nunjucks from 'nunjucks';
 import path from 'path';
+import { z } from 'zod';
+import { fetchWeatherData } from './weatherapi';
+import { fetchLocationData } from './location';
 
 // Get env variables
 dotenv.config();
@@ -97,3 +100,67 @@ const weatherCodeToImage = (code: number): string => {
 			return '/static/img/info.svg';
 	}
 };
+
+const locationSchema = z.object({
+	location: z.string(),
+});
+
+server.get('/', async (request, reply) => {
+	const queryParams = request.query;
+	try {
+		// Check the query parameters has a location property
+		const { location } = locationSchema.parse(queryParams);
+
+		// Fetch location info
+		const locationInfo = await fetchLocationData(
+			HTTP_CLIENT,
+			GEOCODE_API_URL,
+			location
+		);
+
+		// Fetch weather data
+		const weatherInfo = await fetchWeatherData(
+			HTTP_CLIENT,
+			WEATHER_API_URL,
+			locationInfo.lat,
+			locationInfo.lon
+		);
+
+		// Render template
+		const rendered = templates.render('weather.njk', {
+			environment,
+			location: locationInfo.display_name,
+			currentDate: new Date().toDateString(),
+			weather: {
+				...weatherInfo,
+				conditionImg: weatherCodeToImage(weatherInfo.weathercode),
+				condition: weatherInfo.condition(),
+				lowTemp: weatherInfo.lowTemp(),
+				highTemp: weatherInfo.highTemp(),
+			},
+		});
+
+		await reply
+			.header('Content-Type', 'text/html; charset=utf-8')
+			.send(rendered);
+	} catch (error) {
+		console.error(error);
+		const rendered = templates.render('get_started.njk', { environment });
+
+		await reply
+			.header('Content-Type', 'text/html; charset=utf-8')
+			.send(rendered);
+	}
+});
+
+// Start the server
+const start = async (): Promise<void> => {
+	try {
+		await server.listen({ port: 8089 });
+	} catch (error) {
+		server.log.error(error);
+		process.exit(1);
+	}
+};
+
+start();
