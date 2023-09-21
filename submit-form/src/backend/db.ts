8a96a6da6e62eef7c9bffaaf9732834ff8b03bd7
Julien Atanassian <julien.atanassian@gmail.com>
2023-09-20 23:07:40 -0400

    setup database module

---
+++ submit-form/src/backend/db.ts
@@ -0,0 +1,23 @@
+import { AsyncDatabase } from 'promised-sqlite3';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface User {
+	id: number;
+	email: string;
+	hashedPassword: string;
+	agreedToTerms: boolean;
+}
+
+export interface UserRepository {
+	create(user: User): Promise<User>;
+	findByEmail(email: string): Promise<User | undefined>;
+	get(userId: number): Promise<User | undefined>;
+}
+
+export class SqliteUserRepository implements UserRepository {
+	constructor(private readonly db: AsyncDatabase) {}
+
+	create(user: User): Promise<User> {}
+	findByEmail(email: string): Promise<User | undefined> {}
+	get(userId: number): Promise<User | undefined> {}
+}
