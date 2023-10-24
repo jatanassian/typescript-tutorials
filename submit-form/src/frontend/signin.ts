e4aa122e337589b49aad3f396235b465583c4df9
Julien Atanassian <julien.atanassian@gmail.com>
2023-10-24 19:24:01 -0400

    apply validation to signin page

--- submit-form/src/frontend/signin.ts
+++ submit-form/src/frontend/signin.ts
@@ -1,3 +1,5 @@
+import { checkComplexity } from '../shared/password-rules';
+import { checkUsername } from '../shared/username-rules';
 import { FieldError } from './field-error';
 
 const passwordField = document.getElementById('password') as HTMLInputElement;
@@ -15,7 +17,11 @@ const submitBtn = document.getElementById('form-submit');
 const errors = new FieldError();
 
 function updateSubmitButton(): void {
-	if (errors.isEmtpy()) {
+	if (
+		errors.isEmtpy() &&
+		emailField.value.length &&
+		passwordField.value.length
+	) {
 		submitBtn?.classList.remove('btn-disabled');
 	} else {
 		submitBtn?.classList.add('btn-disabled');
@@ -23,9 +29,28 @@ function updateSubmitButton(): void {
 }
 
 emailField.addEventListener('input', () => {
+	const usernameFailures = checkUsername(emailField.value);
+	if (usernameFailures.length) {
+		const formattedErrors = usernameFailures.join('<br>');
+		errors.set('invalid-email', emailField, emailInvalidLabel, formattedErrors);
+	} else {
+		errors.remove('invalid-email', emailField, emailInvalidLabel);
+	}
 	updateSubmitButton();
 });
 
 passwordField.addEventListener('input', () => {
+	const passwordFailures = checkComplexity(passwordField.value);
+	if (passwordFailures.length) {
+		const formattedErrors = passwordFailures.join('<br>');
+		errors.set(
+			'invalid-password',
+			passwordField,
+			passwordInvalidLabel,
+			formattedErrors
+		);
+	} else {
+		errors.remove('invalid-password', passwordField, passwordInvalidLabel);
+	}
 	updateSubmitButton();
 });
