f2c6d9e1e82793189a2c8370d3a0bf153a7b52d9
Julien Atanassian <julien.atanassian@gmail.com>
2023-10-27 16:35:01 -0400

    add signup.ts

---
+++ submit-form/src/frontend/signup.ts
@@ -0,0 +1,68 @@
+import { checkComplexity } from '../shared/password-rules';
+import { checkUsername } from '../shared/username-rules';
+import { FieldError } from './field-error';
+
+const passwordField = document.getElementById('password') as HTMLInputElement;
+const passwordInvalidLabel = document.getElementById(
+	'invalid-password'
+) as HTMLElement;
+
+const emailField = document.getElementById('email') as HTMLInputElement;
+const emailInvalidLabel = document.getElementById(
+	'invalid-email'
+) as HTMLElement;
+
+const agreeTosCheckbox = document.getElementById(
+	'agree-tos'
+) as HTMLInputElement;
+
+const submitBtn = document.getElementById('form-submit');
+
+const errors = new FieldError();
+
+function updateSubmitButton(): void {
+	if (
+		errors.isEmtpy() &&
+		emailField.value.length &&
+		passwordField.value.length &&
+		agreeTosCheckbox.checked
+	) {
+		submitBtn?.classList.remove('btn-disabled');
+	} else {
+		submitBtn?.classList.add('btn-disabled');
+	}
+}
+
+agreeTosCheckbox.addEventListener('change', () => {
+	updateSubmitButton();
+});
+
+emailField.addEventListener('input', () => {
+	const usernameFailures = checkUsername(emailField.value);
+	if (usernameFailures.length) {
+		const formattedErrors = usernameFailures.join('<br>');
+		errors.set('invalid-email', emailField, emailInvalidLabel, formattedErrors);
+	} else {
+		errors.remove('invalid-email', emailField, emailInvalidLabel);
+	}
+	updateSubmitButton();
+});
+
+passwordField.addEventListener('input', () => {
+	const passwordFailures = checkComplexity(passwordField.value);
+	if (passwordFailures.length) {
+		const formattedErrors = passwordFailures.join('<br>');
+		errors.set(
+			'invalid-password',
+			passwordField,
+			passwordInvalidLabel,
+			formattedErrors
+		);
+	} else {
+		errors.remove('invalid-password', passwordField, passwordInvalidLabel);
+	}
+	updateSubmitButton();
+});
+
+// Uncheck the box on page load
+agreeTosCheckbox.checked = false;
